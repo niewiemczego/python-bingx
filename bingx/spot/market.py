ee1db36edec4217deb10f85000d995d4d34d6c23
The Aviator <slotfi909@gmail.com>
2024-04-02 20:10:22 +0430

    add get_k_line_data function to market class to get spot kline data -> (added raise condtion for interval wrong type)

--- bingx/spot/market.py
+++ bingx/spot/market.py
@@ -55,3 +55,26 @@ class Market:
 
         response = self.__http_manager.get(endpoint, payload)
         return response.json()["data"]
+
+    def get_k_line_data(self, symbol: str, interval: str, start_time: int | None = None, end_time: int | None = None, limit: int = 1) -> list[dict[str, Any]] | dict[str, Any]:
+        """
+        Get the latest Kline Data.
+        If startTime and endTime are not sent, the latest k-line data will be returned by default
+
+        :param symbol: The trading pair you want to get the Kline data for
+        :param interval: The interval of the Kline data, possible values: 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 1w, 1M
+        :param start_time: The start time of the Kline data, in milliseconds
+        :param end_time: The end time of the Kline data, in milliseconds
+        :param limit: The number of Kline data to return, maximum 1440
+
+        https://bingx-api.github.io/docs/#/en-us/spot/market-api.html#Candlestick%20chart%20data
+        """
+        VALID_INTERVALS = ["1m", "3m", "5m", "15m", "30m", "1h", "2h", "4h", "6h", "8h", "12h", "1d", "3d", "1w", "1M"]
+        if interval not in VALID_INTERVALS:
+            raise ValueError("[!] INVALID INTERVAL VALUE. Valid Intervals are: ", str(VALID_INTERVALS))
+
+        endpoint = "/openApi/spot/v2/market/kline"
+        payload = {"symbol": symbol.upper(), "interval": interval, "limit": limit} if start_time is None or end_time is None else {"symbol": symbol.upper(), "interval": interval, "startTime": start_time, "endTime": end_time, "limit": limit}
+
+        response = self.__http_manager.get(endpoint, payload)
+        return response.json()["data"]
